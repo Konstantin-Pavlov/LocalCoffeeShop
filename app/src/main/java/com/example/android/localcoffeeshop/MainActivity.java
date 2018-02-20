9ccd07f594ee63d1a04d0d13cedd56747cd6370d
Konstantin-Pavlov <konstpavlov87@gmail.com>
2018-02-20 14:33:16 +0400

    - made app interactive using android:clickable - declaration of variables were moved to the begin of  MainActivity class and initialized them in the onCreate method - disabled screen orientation change using this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); in the onCreate method

--- app/src/main/java/com/example/android/localcoffeeshop/MainActivity.java
+++ app/src/main/java/com/example/android/localcoffeeshop/MainActivity.java
@@ -1,21 +1,46 @@
 package com.example.android.localcoffeeshop;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Double myLatitude, myLongitude;
+    String labelLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
         /*//make the app full screen
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
 
+        myLatitude = 59.922614;
+        myLongitude = 30.310278;
+        labelLocation = getString(R.string.labelLocation);
+
         setContentView(R.layout.activity_main);
+
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
+
+    public void showMap(View v)
+    {
+        //TextView map = (TextView) findViewById(R.id.address);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:<" + myLatitude  + ">,<" + myLongitude + ">?q=<" + myLatitude  + ">,<" + myLongitude + ">(" + labelLocation + ")"));
+        startActivity(intent);
+
+    }
+
 }
